385075fb006d2c4f72f9c2b36839215c1174fa4e
Jón Rúnar Helgason <dev@jonrh.is>
2021-09-04 14:24:32 -0400

    Added endpoints /text, /html, /json along with an index page

--- kek.js
+++ kek.js
@@ -1,7 +1,56 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hæ! 👏👏", {
-    headers: { "content-type": "text/plain" },
-  });
+function handleRequest(request) {
+  if (pathname.startsWith("/text") {
+    return new Response("Hæ! 👏", {
+      headers: { "content-type": "text/plain; charset=UTF-8" }
+    });
+  }
 
-  event.respondWith(response);
-});
+  if (pathname.startsWitdh("/html")) {
+    return new Response(
+      `<p>Basic html, may not work</p>`,
+      {
+	headers: {
+	  "content-type"
+	}
+      }
+    );
+  }
+
+  if (pathname.startsWith("/json")) {
+    const json = JSON.stringify({
+      message: "Hæ! 👏",
+    });
+
+    return new Response(json, {
+      headers: {
+	"content-type": "application/json; charset=UTF-8",
+      }
+    });
+  }
+
+  return new Response(
+    `<html>
+    <head>
+      <title>Deno Deploy testing</title>
+    </head>
+    <body>
+      <h1>Index</h1>
+      <p>Routes:</p>
+      <ul>
+	<li><a href="/text">/text</a></li>
+	<li><a href="/html">/html</a></li>
+	<li><a href="/json">/json</a></li>
+      </ul>
+    </body>
+    </html>`,
+    {
+      headers: {
+	"content-type": "text/html; charset=UTF-8",
+      }
+    }
+  );
+}
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+};
